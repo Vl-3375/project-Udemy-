b0b96667cafe498350406873cdaa89a9b8469aac
Vlad <example@gmail.com>
2024-07-22 13:03:41 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,105 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// console.log(name);
+// var name = 'Ivan';
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+
+// ['vruru', 'ecrvrev', 'vvuguegv'].map(a => )
+
+// Request
+// data
+// Response
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+// let COLOR_RED = '#F00';
+
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false,
+// };
+// obj.gen = 'yes';
+
+// console.log(obj.gen);
+///////////////   Vesta
+// let storeName = 'Vesta';
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Aram', 'Ara', 'Suren'],
+//     products: {
+//         fridge: 4500,
+//         tv: 3500,
+//     },
+//     open: true,
+// };
+
+// console.log(storeDescription.products.fridge);
+
+
+// alert('Hello');
+
+// const result = confirm('Are you here?')
+// console.log(result);
+
+// const answer = +prompt('Сколько вам лет?', '');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`)
+
+// console.log(4 + ' - object');
+
+// let incr = 10,
+//     decr = 10;
+
+//     // ++incr;
+//     // --decr;
+
+//     console.log(++incr);
+//     console.log(--decr);
+
+//     console.log(5%2);
+
+//     console.log(2*4 === 8);
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
